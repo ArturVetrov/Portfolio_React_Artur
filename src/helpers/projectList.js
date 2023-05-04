10097a558c61a107965a8b349db21b71e28aaa0d
Artur <artur97s@yandex.ru>
2023-05-04 11:47:25 +0300

    feat: add new project

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,3 +1,4 @@
+import project00 from './../components/project/img/00.png';
 import project01 from './../components/project/img/01.png';
 import project02 from './../components/project/img/02.png';
 import project03 from './../components/project/img/03.png';
@@ -8,34 +9,40 @@ import project05 from './../components/project/img/05.png';
 const projects = [
   {
     id: '0',
+    title: 'Virtual keyboard',
+    skills: 'Keyboard event, eslint',
+    img: project00,
+    gitHubLink: 'https://github.com/ArturVetrov/virtual_keyboard/tree/development',
+  },{
+    id: '1',
     title: 'House Staff',
     skills: 'React',
     img: project01,
     gitHubLink: 'https://github.com/ArturVetrov/House_Staff_Shop',
   },
   {
-    id: '1',
+    id: '2',
     title: 'Portfolio Artur Vetrov',
     skills: 'React, React-router',
     img: project02,
     gitHubLink: 'https://github.com/ArturVetrov/Portfolio_React_Artur',
   },
   {
-    id: '2',
+    id: '3',
     title: 'Portfolio Denis',
     skills: 'HTML, CSS, mobile',
     img: project03,
     gitHubLink: 'https://github.com/ArturVetrov/Portfolio-Denis-Novik',
   },
   {
-    id: '3',
+    id: '4',
     title: 'Repair Design Project',
     skills: 'HTML, CSS, JavaScript, mobile',
     img: project04,
     gitHubLink: 'https://github.com/ArturVetrov/Portfolio-Denis-Novik',
   },
   {
-    id: '4',
+    id: '5',
     title: 'Cozy House',
     skills: 'HTML, CSS, JavaScript, mobile',
     img: project05,
